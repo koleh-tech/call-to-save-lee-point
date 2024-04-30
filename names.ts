72569641f6027bf200921d420078387fbf1c3cbd
Koleh David <kolehd@gmail.com>
2024-05-01 07:06:36 +0930

    names refactor

---
+++ names.ts
@@ -0,0 +1,14 @@
+// Define types for character and occupation
+export type Character = {
+    name: string
+    occupation: string
+}
+
+export const characters: Character[] = [
+    { name: 'John Doe', occupation: 'Plumber' },
+    { name: 'Jane Smith', occupation: 'Software Developer' },
+    { name: 'xohn Doe', occupation: 'Plumber' },
+    { name: 'xane Smith', occupation: 'Software Developer' },
+    { name: 'Zane Smith', occupation: 'Software Developer' },
+    // Add more characters as needed
+]
